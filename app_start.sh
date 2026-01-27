e8453b8d07b18a8e2bac370a591c2787dd229827
veater <veater@qq.com>
2026-01-27 23:54:17 +0800

    fix: 修正app_start.sh中的shebang语法错误

--- app_start.sh
+++ app_start.sh
@@ -1,2 +1,2 @@
-#/bin/bash
+#!/bin/bash
 java -jar -Djava.security.egd=file:/dev/urandom -Dspring.datasource.url=${DB_URL} -Dspring.datasource.username=${DB_USERNAME} -Dspring.datasource.password=${DB_PASSWORD} /app/app.jar
