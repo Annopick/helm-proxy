80c47fadd884b6a0e9130e2e41b2574d8d319cd0
veater <veater@qq.com>
2026-02-06 19:40:05 +0800

    feat(app): 添加代理服务器配置启动参数
    - 在启动脚本中添加 HTTP 和 HTTPS 代理主机及端口参数
    - 配置无需代理的本地和局域网地址范围
    - 确保数据库连接参数保持不变
    - 优化应用启动时的网络访问环境配置

--- app_start.sh
+++ app_start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-java -jar -Djava.security.egd=file:/dev/urandom -Dspring.datasource.url=${DB_URL} -Dspring.datasource.username=${DB_USERNAME} -Dspring.datasource.password=${DB_PASSWORD} /app/app.jar
+java -jar -Djava.security.egd=file:/dev/urandom -Dhttp.proxyHost=192.168.80.2 -Dhttp.proxyPort=10909 -Dhttps.proxyHost=192.168.80.2 -Dhttps.proxyPort=10909 -Dhttp.nonProxyHosts="127.0.0.1|192.168.*|172.*|*.local" -Dspring.datasource.url=${DB_URL} -Dspring.datasource.username=${DB_USERNAME} -Dspring.datasource.password=${DB_PASSWORD} /app/app.jar
